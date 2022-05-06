9f54c40472afb7227d5253645fbc6fcc9a9c4ea1
Sungjin Kim <human9802@gmail.com>
2022-05-07 01:02:56 +0900

    배포 TEST V.2

--- scripts/start.sh
+++ scripts/start.sh
@@ -37,4 +37,9 @@ echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 
 cd /home/ubuntu/SNSClone
 
-nohup java -jar -Dspring.profiles.active=$IDLE_PROFILE sdd-0.0.1-SNAPSHOT.jar > nohup.out 2>&1 &
+nohup java -jar \
+    -Dspring.config.location=classpath:/application.yml,classpath:/application-$IDLE_PROFILE.propertie \
+    -Dspring.profiles.active=$IDLE_PROFILE \
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+
+#nohup java -jar -Dspring.profiles.active=$IDLE_PROFILE sdd-0.0.1-SNAPSHOT.jar > nohup.out 2>&1 &
